ad13762433923df4f3ba184d10897d031f6f47d2
kazuki120209 <sk83501229@icloud.com>
2021-05-13 00:33:11 +0900

    Initial Commit

---
+++ Shared/kaihatuApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaihatuApp.swift
+//  Shared
+//
+//  Created by 小路一樹 on 2021/05/13.
+//
+
+import SwiftUI
+
+@main
+struct kaihatuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
